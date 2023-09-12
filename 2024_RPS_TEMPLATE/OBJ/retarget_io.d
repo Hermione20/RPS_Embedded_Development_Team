02fbeb3a3fb2a7cf1b1237213d83e0465ff4e5a8
拂晓曙色之前 <ghy0407221@Outlook.com>
2023-09-12 23:58:52 +0800

    delete

--- 2024_RPS_TEMPLATE/OBJ/retarget_io.d
+++
@@ -1,8 +0,0 @@
-..\obj\retarget_io.o: E:\Keil_v5\ARM\PACK\Keil\ARM_Compiler\1.3.0\Source\retarget_io.c
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\assert.h
-..\obj\retarget_io.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_sys.h
-..\obj\retarget_io.o: .\RTE\_Template\RTE_Components.h
