c698ec38ff94f102d2483286fa3989ad7af4f6b2
LeeZekai233 <115407059+LeeZekai233@users.noreply.github.com>
2023-09-30 18:08:45 +0800

    3.0

---
+++ DEMO/EMBEDDED/senior/DJI_MOTOR/DJI_MOTOR.c
@@ -0,0 +1,106 @@
+#include "DJI_MOTOR.h"
+
+
+/*****************************dji encoder*************************************/
+
+void GetEncoderBias(volatile Encoder *v, CanRxMsg * msg)
+{
+
+            v->ecd_bias = (msg->Data[0]<<8)|msg->Data[1];  //保存初始编码器值作为偏差  
+            v->ecd_value = v->ecd_bias;
+            v->last_raw_value = v->ecd_bias;
+            v->temp_count++;
+}
+
+void EncoderProcess(volatile Encoder *v, CanRxMsg * msg)
+{
+	int i=0;
+	int32_t temp_sum = 0;    
+	v->last_raw_value = v->raw_value;
+	v->raw_value = (msg->Data[0]<<8)|msg->Data[1];
+	v->diff = v->raw_value - v->last_raw_value;
+	if(v->diff < -4096)    //两次编码器的反馈值差别太大，表示圈数发生了改变
+	{
+		v->round_cnt++;
+		v->ecd_raw_rate = v->diff + 8192;
+	}
+	else if(v->diff>4096)
+	{
+		v->round_cnt--;
+		v->ecd_raw_rate = v->diff- 8192;
+	}		
+	else
+	{
+		v->ecd_raw_rate = v->diff;
+	}
+	//计算得到连续的编码器输出值
+	v->ecd_value = v->raw_value + v->round_cnt * 8192;
+	//计算得到角度值，范围正负无穷大
+	v->ecd_angle = (float)(v->raw_value - v->ecd_bias)*0.04394531f + v->round_cnt * 360;
+	v->rate_buf[v->buf_count++] = v->ecd_raw_rate;
+	if(v->buf_count == RATE_BUF_SIZE)
+	{
+		v->buf_count = 0;
+	}
+	//计算速度平均值
+	for(i = 0;i < RATE_BUF_SIZE; i++)
+	{
+		temp_sum += v->rate_buf[i];
+	}
+	v->filter_rate = (int32_t)(temp_sum/RATE_BUF_SIZE);		
+	v->rate_rpm = (msg->Data[2]<<8)|msg->Data[3];
+}
+
+void GM6020EncoderProcess(volatile Encoder *v, CanRxMsg * msg)
+{
+	v->last_raw_value = v->raw_value;
+	v->raw_value = (msg->Data[0]<<8)|msg->Data[1];
+	v->diff = v->raw_value - v->last_raw_value;
+	if(v->diff < -4096)    //两次编码器的反馈值差别太大，表示圈数发生了改变
+	{
+		v->round_cnt++;
+		v->ecd_raw_rate = v->diff + 8192;
+	}
+	else if(v->diff>4096)
+	{
+		v->round_cnt--;
+		v->ecd_raw_rate = v->diff- 8192;
+	}		
+	else
+	{
+		v->ecd_raw_rate = v->diff;
+	}
+	v->ecd_value = v->raw_value + v->round_cnt * 8192;
+	//计算得到角度值，范围正负无穷大
+	v->ecd_angle = (float)(v->raw_value - v->ecd_bias)*0.0439453125f  + v->round_cnt * 360;
+	v->filter_rate = (msg->Data[2]<<8)|msg->Data[3];
+	if(v->filter_rate>=1000)
+		v->filter_rate = v->filter_rate - 65535;
+	v->temperature = msg->Data[6];
+}
+
+
+void M3508orM2006EncoderTask(uint32_t can_count,volatile Encoder *v, CanRxMsg * msg)
+{
+	(can_count<=50)?GetEncoderBias(v,msg):EncoderProcess(v,msg);
+}
+
+
+void GM6020EncoderTask(uint32_t can_count,volatile Encoder *v, CanRxMsg * msg,int offset)
+{
+
+	GM6020EncoderProcess(v, msg);
+	// 码盘中间值设定也需要修改
+	if (can_count <= 100)
+	{
+		if ((v->ecd_bias - v->ecd_value) < -4000)
+		{
+				v->ecd_bias = offset + 8192;
+		}
+		else if ((v->ecd_bias - v->ecd_value) > 4000)
+		{
+				v->ecd_bias = offset - 8192;
+		}
+	}
+}
+
