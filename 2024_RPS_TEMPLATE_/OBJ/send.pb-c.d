e6e8b2135507d5940865201a9ea8ae60d387eff8
LeeZekai233 <115407059+LeeZekai233@users.noreply.github.com>
2023-10-13 14:04:23 +0800

    add gitignore

--- 2024_RPS_TEMPLATE_/OBJ/send.pb-c.d
+++
@@ -1,7 +0,0 @@
-..\obj\send.pb-c.o: ..\EMBEDDED\senior\Auto_Shoot\proto\Send.pb-c.c
-..\obj\send.pb-c.o: ..\EMBEDDED\senior\Auto_Shoot\proto\Send.pb-c.h
-..\obj\send.pb-c.o: ..\EMBEDDED\senior\Auto_Shoot\proto\protobuf-c.h
-..\obj\send.pb-c.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\assert.h
-..\obj\send.pb-c.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
-..\obj\send.pb-c.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-..\obj\send.pb-c.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
